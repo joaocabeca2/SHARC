caf423eb0b8470960b0ea8d07322a6db54d6191f
Daniel Costa Araújo <63726804+araujoUnb@users.noreply.github.com>
2024-04-23 17:06:03 -0300

    refactoring parameter_hotspot

--- sharc/parameters/parameters_hotspot.py
+++ sharc/parameters/parameters_hotspot.py
@@ -1,11 +1,32 @@
-# -*- coding: utf-8 -*-
+"""Parameters definitions for Hotopsot systems
 """
-Created on Wed May 17 15:47:05 2017
+import configparser
+from dataclasses import dataclass
 
-@author: edgar
-"""
+from sharc.parameters.parameters_base import ParametersBase
 
-class ParametersHotspot(object):
+@dataclass
 
-    def __init__(self):
-        pass
+class ParametersHotspot(ParametersBase):
+    """Dataclass containing the Hotspot system parameters
+    """
+
+    num_hotspots_per_cell = 1
+    max_dist_hotspot_ue   = 100
+    min_dist_bs_hotspot   = 0
+
+
+    def load_parameters_from_file(self, config_file: str):
+        """Load the parameters from file an run a sanity check
+
+        Parameters
+        ----------
+        file_name : str
+            the path to the configuration file
+
+        Raises
+        ------
+        ValueError
+            if a parameter is not valid
+        """
+        super().load_parameters_from_file(config_file)
