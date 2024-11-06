61f6f2211b67ace647269d342d48bde740788e9c
artistrea <apiravesi@gmail.com>
2024-11-06 15:27:44 -0300

    update: better campaign imt hotspot eess active

--- sharc/campaigns/imt_hotspot_eess_active/readme.md
+++ sharc/campaigns/imt_hotspot_eess_active/readme.md
@@ -2,9 +2,5 @@
 
 This campaign's objective is to try and replicate Luciano's contribution 20.
 
-As such, the `generate_aggregate_cdf.py` and `plot_comparison.py` were made.
-
-After running both uplink and downlink simulations,
-you can generate an aggregate cdf based on the TDD and segment factors with `generate_aggregate_cdf.py`. The script also copies samples in the `comparison` folder to the `output` folder as CDF's of System INR.
-
-You can then compare SHARC results against Luciano's results with a simple `plot_results.py`.
+Using the script `plot_results.py` automatically aggregates results and compares it to Luciano's INR results in
+contrib 21
--- sharc/campaigns/imt_hotspot_eess_active/scripts/plot_results.py
+++ sharc/campaigns/imt_hotspot_eess_active/scripts/plot_results.py
@@ -1,111 +1,77 @@
 import os
-from sharc.plots.plot_cdf import plot_system_inr
+from pathlib import Path
+from sharc.results import Results
+import plotly.graph_objects as go
+from sharc.post_processor import PostProcessor
+import pandas
 
-# Define the base directory
-name = "imt_hotspot_eess_active"
+post_processor = PostProcessor()
 
-# this should behave similarly to `sharc/plots/plot_cdf:13`
-# ideally the readable legend would be in the .ini metadata and all this code would be deleted
-workfolder = os.path.dirname(os.path.abspath(__file__))
-csv_folder = os.path.abspath(os.path.join(workfolder, "..", "output"))
+# Add a legend to results in folder that match the pattern
+# This could easily come from a config file
+post_processor\
+    .add_plot_legend_pattern(
+        dir_name_contains="beam_small_dl",
+        legend="Small Beam DL"
+    ).add_plot_legend_pattern(
+        dir_name_contains="beam_small_ul",
+        legend="Small Beam UL"
+    )
 
-subdirs = [
-    d
-    for d in os.listdir(csv_folder)
-    if os.path.isdir(os.path.join(csv_folder, d))
-    and d.startswith(f"output_{name}_")
-]
+campaign_base_dir = str((Path(__file__) / ".." / "..").resolve())
 
+many_results = Results.load_many_from_dir(os.path.join(campaign_base_dir, "output"), only_latest=True)
 
-def get_date_from_dirname(dirname: str, prefix_length: int):
-    return dirname[prefix_length : prefix_length + len("yyyy-mm-dd")]
+post_processor.add_results(many_results)
 
-def get_id_from_dirname(dirname: str):
-    return dirname.split("_")[-1]
-
-legends_mapper = [
-    {
-        # same as from .ini
-        # the legend should probably also be in the metadata there instead of making... this
-        "directory_pattern": "beam_small_dl",
-        "legend": "Small Beam DL",
-    },
-    {
-        "directory_pattern": "beam_small_ul",
-        "legend": "Small Beam UL",
-    },
-    {
-        "directory_pattern": "contribution_20",
-        "legend": "Contribution 20 INR",
-    },
-    {
-        "directory_pattern": "aggregate",
-        "legend": "Aggregate INR",
-    },
-]
-
-def get_id_from_dirname(dirname: str):
-    return dirname.split("_")[-1]
-
-
-subfolders_filters = {}
-
-for subdir in subdirs:
-    for mapper in legends_mapper:
-        if mapper["directory_pattern"] in subdir:
-            subfolders_filters\
-                .setdefault(mapper["directory_pattern"], { "id": "0", "date": "2024-01-01" })
-
-            subfolders_filters[mapper["directory_pattern"]]["id"] = max(
-                        subfolders_filters[mapper["directory_pattern"]]["id"],
-                        get_id_from_dirname(subdir)
-                    )
-            subfolders_filters[mapper["directory_pattern"]]["date"] = max(
-                        subfolders_filters[mapper["directory_pattern"]]["date"],
-                        get_date_from_dirname(subdir, 1 + len(mapper['directory_pattern']))
-                    )
-
-
-legend_and_subfolders = [
-    {
-        # "legend": f"{mapper['legend']} - ({get_date_from_dirname(d, 1 + len(mapper['directory_pattern']))}) {get_id_from_dirname(d)}",
-        "legend": f"{mapper['legend']}",
-        "subfolder": d,
-    }
-    for d in subdirs
-    for mapper in legends_mapper
-    if mapper["directory_pattern"] in d
-        # comment filters out if needed
-        and subfolders_filters[mapper["directory_pattern"]]["id"] == get_id_from_dirname(d)
-        and subfolders_filters[mapper["directory_pattern"]]["date"] == get_date_from_dirname(d, len(mapper["directory_pattern"]) + 1)
-]
-
-# Example with specific subfolders and legends
-# Define legend names for the different subdirectories
-# legends = None
-legends = [x["legend"] for x in legend_and_subfolders]
-
-# Define specific subfolders if needed
-# subfolders = None
-subfolders = [x["subfolder"] for x in legend_and_subfolders]
-
-# Run the function with specific subfolders and legends
-plot_system_inr(
-    name,
-    legends=legends,
-    subfolders=subfolders,
-    save_file=False,
-    show_plot=True,
+plots = post_processor.generate_cdf_plots_from_results(
+    many_results
 )
 
+post_processor.add_plots(plots)
 
-# Example with all subfolders and no specific legends
-# This will include all subfolders that start with "output_imt_hibs_ras_2600_MHz_" in the base directory
-# and generate legends automatically based on the folder names
+# This function aggregates IMT downlink and uplink
+aggregated_results = PostProcessor.aggregate_results(
+    dl_samples=post_processor.get_results_by_output_dir("_dl").system_inr,
+    ul_samples=post_processor.get_results_by_output_dir("_ul").system_inr,
+    ul_tdd_factor=0.25,
+    # SF is not exactly 1, but approx
+    n_bs_sim=1,
+    n_bs_actual=1
+)
 
-# Define legends and subfolders as None to include all automatically
-# legends = None
-# subpastas = None
+# Add a protection criteria line:
+# protection_criteria = int
 
-# Run the function with all subfolders and auto-generated legends
-# plot_system_inr(name, legends=legends, subpastas=subpastas, save_file=True, show_plot=False)
+# post_processor\
+#     .get_plot_by_results_attribute_name("system_dl_interf_power")\
+#     .add_vline(protection_criteria, line_dash="dash")
+
+# Show a single plot:
+relevant = post_processor\
+    .get_plot_by_results_attribute_name("system_inr")
+
+aggr_x, aggr_y = PostProcessor.cdf_from(aggregated_results)
+
+relevant.add_trace(
+    go.Scatter(x=aggr_x, y=aggr_y, mode='lines', name='Aggregate interference',),
+)
+
+compare_to = pandas.read_csv(
+    os.path.join(campaign_base_dir, "comparison", "contribution_20.csv"),
+    skiprows=1
+)
+
+comp_x, comp_y = (compare_to.iloc[:, 0], compare_to.iloc[:, 1])
+
+relevant.add_trace(
+    go.Scatter(x=comp_x, y=comp_y, mode='lines', name='Contrib 20 INR',),
+)
+
+relevant.show()
+
+for result in many_results:
+    # This generates the mean, median, variance, etc
+    stats = PostProcessor.generate_statistics(
+        result=result
+    ).write_to_results_dir()
