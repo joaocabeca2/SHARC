0861016e17342c1a2e486a10b7f4773f5458cebb
Bruno Faria <brunohcfaria@gmail.com>
2024-12-10 15:14:24 -0300

    Implemented campaign running script which accepts a regex for input paramters

--- sharc/campaigns/imt_ntn_to_imt_tn_co_channel/scripts/start_simulations_single_thread.py
+++ sharc/campaigns/imt_ntn_to_imt_tn_co_channel/scripts/start_simulations_single_thread.py
@@ -1,4 +1,4 @@
-from sharc.run_multiple_campaigns import run_campaign
+from sharc.run_multiple_campaigns import run_campaign_re
 
 # Set the campaign name
 # The name of the campaign to run. This should match the name of the campaign directory.
@@ -7,4 +7,4 @@ name_campaign = "imt_ntn_to_imt_tn_co_channel"
 # Run the campaign in single-thread mode
 # This function will execute the campaign with the given name in a single-threaded manner.
 # It will look for the campaign directory under the specified name and start the necessary processes.
-run_campaign(name_campaign)
+run_campaign_re(name_campaign, r'^parameters_imt_ntn_to_imt_tn_no_overlap_.*_alt_500km_sep_0km.yaml')
