7312c5657ac9484ee0a1d8005fbc2ee1212fdde8
artistrea <apiravesi@gmail.com>
2025-03-24 10:38:44 -0300

    update: parameters for beamsteering range validation

--- sharc/parameters/imt/parameters_imt.py
+++ sharc/parameters/imt/parameters_imt.py
@@ -70,6 +70,14 @@ class ParametersImt(ParametersBase):
         body_loss: float = 4.0
         antenna: ParametersAntennaImt = field(default_factory=lambda: ParametersAntennaImt(downtilt=0.0,))
 
+        def validate(self, ctx: str):
+            if self.antenna.horizontal_beamsteering_range != (-180., 180.)\
+                    or self.antenna.vertical_beamsteering_range != (0., 180.):
+                raise NotImplementedError(
+                    "UE antenna beamsteering limit has not been implemented. Default values of\n"
+                    "horizontal = (-180., 180.), vertical = (0., 180.) should not be changed"
+                )
+
     ue: ParametersUE = field(default_factory=ParametersUE)
 
     @dataclass
