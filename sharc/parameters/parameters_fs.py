e03bdd727279de5cbd00b1e511a9842fe339deda
achilesfontanamota <162994870+achilesfontanamota@users.noreply.github.com>
2025-03-25 18:01:10 -0300

    Change in Clutter Loss function and Addition possible P452 as propagation for FS (#129)
    * Fixing Clutter Loss for >250m
    
    * Add P452 as a propagation model for FS
    ---------
    
    Co-authored-by: Achiles <achiles.mota@ene.unb.com>

--- sharc/parameters/parameters_fs.py
+++ sharc/parameters/parameters_fs.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from sharc.parameters.parameters_base import ParametersBase
+from sharc.parameters.parameters_p452 import ParametersP452
 
 
 @dataclass
@@ -49,8 +50,38 @@ class ParametersFs(ParametersBase):
     diameter: float = 0.3
 
     # Channel model, possible values are "FSPL" (free-space path loss),
-    # "TerrestrialSimple" (FSPL + clutter loss)
+    # "TerrestrialSimple" (FSPL + clutter loss),
+    # P452
     channel_model: str = "FSPL"
+
+    # P452 parameters
+    param_p452 = ParametersP452()
+    # Total air pressure in hPa
+    atmospheric_pressure: float = 935.0
+    # Temperature in Kelvin
+    air_temperature: float = 300.0
+    # Sea-level surface refractivity (use the map)
+    N0: float = 352.58
+    # Average radio-refractive (use the map)
+    delta_N: float = 43.127
+    # Percentage p. Float (0 to 100) or RANDOM
+    percentage_p: str = "0.2"
+    # Distance over land from the transmit and receive antennas to the coast (km)
+    Dct: float = 70.0
+    # Distance over land from the transmit and receive antennas to the coast (km)
+    Dcr: float = 70.0
+    # Effective height of interfering antenna (m)
+    Hte: float = 20.0
+    # Effective height of interfered-with antenna (m)
+    Hre: float = 3.0
+    # Latitude of transmitter
+    tx_lat: float = -23.55028
+    # Latitude of receiver
+    rx_lat: float = -23.17889
+    # Antenna polarization
+    polarization: str = "horizontal"
+    # Determine whether clutter loss following ITU-R P.2108 is added (TRUE/FALSE)
+    clutter_loss: bool = True
 
     def load_parameters_from_file(self, config_file: str):
         """
@@ -85,7 +116,9 @@ class ParametersFs(ParametersBase):
             )
 
         # Sanity check for channel model
-        if self.channel_model not in ["FSPL", "TerrestrialSimple"]:
+        if self.channel_model not in ["FSPL", "TerrestrialSimple", "P452"]:
             raise ValueError(
-                "Invalid channel_model, must be either 'FSPL' or 'TerrestrialSimple'",
+                "Invalid channel_model, must be either 'FSPL', 'TerrestrialSimple', or 'P452'",
             )
+        if self.channel_model == "P452":
+            self.param_p452.load_from_paramters(self)
