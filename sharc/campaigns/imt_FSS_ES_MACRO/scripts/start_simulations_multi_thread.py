eb092fe5882b0a3721cb0d2684ab3f73eee27980
LeoAguayoUnB <44711039+LeoAguayoUnB@users.noreply.github.com>
2025-04-26 09:40:10 -0300

    novas campanhas

---
+++ sharc/campaigns/imt_FSS_ES_MACRO/scripts/start_simulations_multi_thread.py
@@ -0,0 +1,10 @@
+from sharc.run_multiple_campaigns_mut_thread import run_campaign
+
+# Set the campaign name
+# The name of the campaign to run. This should match the name of the campaign directory.
+name_campaign = "imt_hibs_ras_2600_MHz"
+
+# Run the campaigns
+# This function will execute the campaign with the given name.
+# It will look for the campaign directory under the specified name and start the necessary processes.
+run_campaign(name_campaign)
