f6af3f202e07881a1bf90f3dab8e602e42eabb14
Bruno Faria <brunohcfaria@gmail.com>
2025-05-20 17:36:00 -0300

    Removed outdated parameters for systems that falls into single space station or single earth station categories.

--- sharc/sharc_definitions.py
+++ sharc/sharc_definitions.py
@@ -1,13 +1,6 @@
 """Commom SHARC definitions"""
 SHARC_IMPLEMENTED_SYSTEMS = [
-# TODO: actually remove implementations if no one is using
-# these definitions:
-    # "EESS_SS",
-    # "METSAT_SS",
-    # "FSS_SS",
-    # "FSS_ES",
-    # "FS",
-    # "RAS",
+    "HAPS",
     "MSS_SS",
     "SINGLE_EARTH_STATION",
     "MSS_D2D",
