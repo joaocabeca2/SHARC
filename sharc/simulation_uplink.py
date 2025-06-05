a1a6cb851019bab82d042c4cbd6b3cc397c854a8
artistrea <apiravesi@gmail.com>
2025-06-05 00:22:05 -0300

    update: imt DL victim adj channel simulation works the same way as UL

--- sharc/simulation_uplink.py
+++ sharc/simulation_uplink.py
@@ -178,19 +178,80 @@ class SimulationUplink(Simulation):
         oob_power = -500
         oob_interf_lin = 0
         if self.adjacent_channel:
-            if self.parameters.imt.adjacent_interf_model == "SPECTRAL_MASK":
-                # Out-of-band power in the adjacent channel.
-                oob_power = self.system.spectral_mask.power_calc(self.parameters.imt.frequency,
-                                                                 self.parameters.imt.bandwidth)
-                oob_interf_lin = np.power(10, 0.1 * oob_power) / \
-                    np.power(10, 0.1 * self.parameters.imt.bs_adjacent_ch_selectivity)
-            elif self.parameters.imt.adjacent_interf_model == "ACIR":
-                acir = -10 * np.log10(10**(-self.param_system.adjacent_ch_leak_ratio / 10) +
-                                      10**(-self.parameters.imt.bs_adjacent_ch_selectivity / 10))
-                oob_power = self.param_system.tx_power_density + \
+            # emissions outside of tx bandwidth and inside of rx bw
+            # due to oob emissions on tx side
+            tx_oob = -500
+
+            # emissions outside of rx bw and inside of tx bw
+            # due to non ideal filtering on rx side
+            # will be the same for all UE's, only considering
+            rx_oob = -500
+
+            # TODO: M.2101 states that:
+            # "The ACIR value should be calculated based on per UE allocated number of resource blocks"
+
+            # should we actually implement that for ACS since the receiving filter is fixed?
+
+            # or maybe ignore ACS altogether (ACS = inf)? If we consider only allocated RB, it makes
+            # no sense to use ACS.
+            # At the same time, ignoring ACS doesn't seem correct since the interference
+            # could DECREASE when it would make sense for it to increase.
+            # e.g. adjacent systems -> slightly co-channel with ACS = inf
+            # should interfer ^        less than this ^
+
+            # Unless we never use ACS..?
+            if self.parameters.imt.adjacent_ch_reception == "ACS":
+                if self.overlapping_bandwidth:
+                    if not hasattr(self, "ALREADY_WARNED_ABOUT_ACS_WHEN_OVERLAPPING_BAND"):
+                        print(
+                            "[WARNING]: You're trying to use ACS on a partially overlapping band"
+                            "with UEs. Verify the code implements the behavior you expect"
+                        )
+                        self.ALREADY_WARNED_ABOUT_ACS_WHEN_OVERLAPPING_BAND = True
+                # only apply ACS over non overlapping bw
+                p_tx = self.param_system.tx_power_density \
+                        + 10 * np.log10(
+                            (self.param_system.bandwidth - self.overlapping_bandwidth) * 1e6
+                        )
+
+                rx_oob = p_tx - self.parameters.imt.bs.adjacent_ch_selectivity
+            elif self.parameters.imt.adjacent_ch_reception == "OFF":
+                pass
+            else:
+                raise ValueError(
+                    f"No implementation for parameters.imt.adjacent_ch_reception == {self.parameters.imt.adjacent_ch_reception}"
+                )
+
+            # for tx oob we accept ACLR and spectral mask
+            if self.param_system.adjacent_ch_emissions == "SPECTRAL_MASK":
+                tx_oob = self.system.spectral_mask.power_calc(
+                    self.parameters.imt.frequency,
+                    self.parameters.imt.bandwidth * (
+                        1 - self.parameters.imt.guard_band_ratio
+                    )
+                ) - 30
+            elif self.param_system.adjacent_ch_emissions == "ACLR":
+                # consider ACLR only over non co-channel RBs
+                # This should diminish some of the ACLR interference
+                # in a way that make sense
+                tx_oob = self.param_system.tx_power_density + \
                     10 * np.log10(self.param_system.bandwidth * 1e6) -  \
-                    acir + 30
-                oob_interf_lin = 10**(oob_power / 10)
+                    self.param_system.adjacent_ch_leak_ratio
+            elif self.param_system.adjacent_ch_emissions == "OFF":
+                pass
+            else:
+                raise ValueError(
+                    f"No implementation for param_system.adjacent_ch_emissions == {self.param_system.adjacent_ch_emissions}"
+                )
+
+            # Out of band power
+            # sum linearly power leaked into band and power received in the adjacent band
+
+            oob_power = 10 * np.log10(
+                10 ** (0.1 * tx_oob) + 10 ** (0.1 * rx_oob)
+            )
+            # repeat ue received power for each coupling loss
+            oob_interf_lin = np.power(10, 0.1 * oob_power)
 
         ext_interference = 10 * np.log10(np.power(10, 0.1 * in_band_interf) + oob_interf_lin)
 
